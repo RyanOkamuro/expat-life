5eea5719ccb2aa853484819700d535e2042a7465
Ryan Okamuro <ryaniscool003@yahoo.com>
2018-10-18 17:40:26 -0700

    Add scss files

--- src/components/CustomNavBar.jsx
+++ src/components/CustomNavBar.jsx
@@ -21,14 +21,14 @@ class CustomNavBar extends Component {
                         <NavItem eventKey={2} componentClass={Link} href="/about" to="/about">
                             About
                         </NavItem>
-                        <NavItem eventKey={3} componentClass={Link} href="/expat" to="/expat">
-                            Working Abroad
+                        <NavItem eventKey={3} componentClass={Link} href="/work-abroad" to="/work-abroad">
+                            Work Abroad
                         </NavItem>
                         <NavItem eventKey={4} componentClass={Link} href="/study-abroad" to="/study-abroad">
                             Study Abroad
                         </NavItem>
-                        <NavItem eventKey={5} componentClass={Link} href="/traveling" to="/traveling">
-                            Traveling
+                        <NavItem eventKey={5} componentClass={Link} href="/travel" to="/travel">
+                            Travel
                         </NavItem>
                     </Nav>
                 </Navbar.Collapse>
