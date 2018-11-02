c32b803e7648e140f162902735013ee39428f55f
Ryan Okamuro <ryaniscool003@yahoo.com>
2018-11-01 17:15:49 -0700

    Add aricle date

--- src/components/blog-article/ArticleMain.jsx
+++ src/components/blog-article/ArticleMain.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Moment from 'react-moment';
 import './ArticleMain.css'
 
 class ArticleMain extends Component {
@@ -6,6 +7,7 @@ class ArticleMain extends Component {
         return (
             <div className='blog-main-article'>
                 <h2 className='blog-header'>{this.props.blogPosts.title}</h2>
+                <h3 className='date-created'>Posted: <Moment format='MMMM, DD YYYY'>{this.props.blogPosts.createdAt}</Moment></h3>
                 <img src={this.props.blogPosts.image} className='blog-post-image' alt='blog-post-img'></img>
                 <p className='blog-article-content'>{this.props.blogPosts.blogEntry}</p>
             </div>
