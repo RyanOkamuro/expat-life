76ab12d53121475a35fac2bfa8df5276134b6203
Ryan Okamuro <ryaniscool003@yahoo.com>
2018-11-02 16:44:16 -0700

    Add line break for blog articles

--- src/components/blog-article/ArticleMain.jsx
+++ src/components/blog-article/ArticleMain.jsx
@@ -4,12 +4,15 @@ import './ArticleMain.css'
 
 class ArticleMain extends Component {
     render() {
+        let BlogArticle = this.props.blogPosts.blogEntry
         return (
             <div className='blog-main-article'>
                 <h2 className='blog-header'>{this.props.blogPosts.title}</h2>
                 <h3 className='date-created'>Posted: <Moment format='MMMM, DD YYYY'>{this.props.blogPosts.createdAt}</Moment></h3>
                 <img src={this.props.blogPosts.image} className='blog-post-image' alt='blog-post-img'></img>
-                <p className='blog-article-content'>{this.props.blogPosts.blogEntry}</p>
+                {BlogArticle.split('\n').map(paragraph => {
+                    return (<p className='blog-article-content'>{paragraph}</p>)
+                })}
             </div>
         );
     }
