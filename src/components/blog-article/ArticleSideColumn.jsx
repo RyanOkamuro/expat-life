0f4bd976331b9f29fa4bfcda60b28e1c77fa03f6
Ryan Okamuro <ryaniscool003@yahoo.com>
2018-11-29 18:58:26 -0800

    Update Instagram feed link and format

--- src/components/blog-article/ArticleSideColumn.jsx
+++ src/components/blog-article/ArticleSideColumn.jsx
@@ -7,6 +7,7 @@ class ArticleSideColumn extends Component {
         return (
             <div className='article-side-column-container'>
                 <h4 className='instagram-title'>Instagram Feed</h4>
+                <h3 className='instagram-follow-me'><a href='https://www.instagram.com/gogoryano/'>Follow me: @gogoryano</a></h3>
                 <Instagram />
             </div>
         );
