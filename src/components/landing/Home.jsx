6b95a21ec1e815933f99acf7fc505971b3696780
Ryan Okamuro <ryaniscool003@yahoo.com>
2018-12-03 17:37:54 -0800

    Add landing profile pic and about animation

--- src/components/landing/Home.jsx
+++ src/components/landing/Home.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import BloggingLanding from './BloggingLanding';
 import BannerCarousel from '../banner/BannerCarousel';
+import Bounce from 'react-reveal/Bounce';
 import {getAllBlogEntries} from '../../actions/blogActions';
 import {connect} from 'react-redux';
 import {withRouter} from 'react-router-dom';
@@ -23,12 +24,16 @@ class Home extends Component {
             <div className='home-page'>
                 <BannerCarousel />
                 <section className='landing-profile'>
-                    <img src='https://github.com/RyanOkamuro/expat-life/blob/master/public/assets/Me/Me.jpg?raw=true' className='profile-landing-pic' alt='profile-landing-pic'></img>
+                    <Bounce left show>
+                        <img src='https://github.com/RyanOkamuro/expat-life/blob/master/public/assets/Me/Me.jpg?raw=true' className='profile-landing-pic' alt='profile-landing-pic'></img>
+                    </Bounce>
                     <div className='landing-about'>
-                        <h1>Welcome to the Expat</h1> 
-                        <p className='landing-bio'>Since 2009, I have been coming back and forth from living abroad.  
-                        In total, it has been 6 years.  The experiences I had from meeting new people to understanding a 
-                        different culture changed my perspective on life.</p>
+                        <Bounce left show>
+                            <h1>Welcome to the Expat</h1> 
+                            <p className='landing-bio'>Since 2009, I have been coming back and forth from living abroad.  
+                            In total, it has been 6 years.  The experiences I had from meeting new people to understanding a 
+                            different culture changed my perspective on life.</p>
+                        </Bounce>
                     </div>
                 </section>
                 <section className='landing-featured-article-container'>
